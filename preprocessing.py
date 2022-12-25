e9286b58ef77a0ba7bab8c43b44b1b8cc563fb5e
asus <mehdimohajeri78@gmail.com>
2022-12-25 18:00:52 +0330

    Update regex pattern for removing english words

--- preprocessing.py
+++ preprocessing.py
@@ -19,18 +19,18 @@ class Preprocessing:
         return pattern.sub('', text)
     
     def remove_english_words(self, text):
-        return  re.sub('\s*[A-Za-z]+\b', '' , text)
+        return re.sub(r'[A-Za-z]+', '' , text)
 
     def remove_hashtags(self, text):
-        text = re.sub("#[A-Za-z0-9_]+","", text)
-        text = re.sub("#[\u06F0-\u06F9\u0660-\u0669\u0621-\u0628\u062A-\u063A\u0641-\u0642\u0644-\u0648\u064E-\u0651\u0655\u067E\u0686\u0698\u06A9\u06AF\u06BE\u06CC_]+","", text)
+        text = re.sub(r"#[A-Za-z0-9_]+","", text)
+        text = re.sub(r"#[\u06F0-\u06F9\u0660-\u0669\u0621-\u0628\u062A-\u063A\u0641-\u0642\u0644-\u0648\u064E-\u0651\u0655\u067E\u0686\u0698\u06A9\u06AF\u06BE\u06CC_]+","", text)
         return text
     
     def remove_specific_character(self, text, char):
         return text.replace(char, '')
 
     def remove_mentions(self, text):
-        return re.sub("@[A-Za-z0-9_]+","", text)
+        return re.sub(r"@[A-Za-z0-9_]+","", text)
 
     def sentence_tokenize(self, text):
         tokenizer = hazm.SentenceTokenizer()
