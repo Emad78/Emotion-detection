bddae63af5db0404a0a774f480637e9af5f80bfd
asus <mehdimohajeri78@gmail.com>
2022-12-25 15:23:40 +0330

    Define preprocessing structure

---
+++ preprocessing.py
@@ -0,0 +1,35 @@
+import hazm
+import re
+
+class Preprocessing:
+    def __init__(self):
+        pass
+    def normalize(self, text):
+        pass
+
+    def remove_emojis(self, text):
+        pass
+    
+    def remove_english_words(self, text):
+        pass
+
+    def remove_hashtags(self, text):
+        pass
+
+    def remove_special_character(self, text, char):
+        pass
+
+    def remove_mentions(self, text):
+        pass
+
+    def sentence_tokenize(self, text):
+        pass
+
+    def word_tokenize(self, sentence):
+        pass
+
+    def remove_stop_words(self, words):
+        pass
+
+    def remove_char_duplicates(self, word):
+        pass
